5c3ee9123d991678ae8aebd5ac6721f75a8fed47
Dame <dcampbell7095@gmail.com>
2020-10-11 17:28:34 -0400

    adding code to add city information to card along with appending recent searches to an unordered list

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
 
 
+
     function getCity(userCity){
     
         var queryURL =
           "https://api.openweathermap.org/data/2.5/weather?q=" +
           userCity +
-          "&appid=1595add4d1ccc2ce38dfcf973ec248ec";
+          "&appid=1595add4d1ccc2ce38dfcf973ec248ec&units=imperial";
     
         $.ajax({
           url: queryURL,
@@ -20,8 +21,14 @@ $(document).ready(function () {
 
     function displayCityWeather(arrayOfWeather){
 
-        $("#cityInfo").text(arrayOfWeather.name)
+        $("#cityInfo").text(arrayOfWeather.name);
+        $("#temparature").text("Temparature: " + arrayOfWeather.main.temp);
+        $("#humidity").text("Humidity: " + arrayOfWeather.main.humidity);
+        $("#wind-speed").text("Wind-Speed: " + arrayOfWeather.wind.speed);
+        // $("#uv-index").text("UV Index: " + arrayOfWeather.main.temp);
+
     }
+
 
 
   $("#submitBtn").on("click", function (event) {
@@ -29,6 +36,13 @@ $(document).ready(function () {
 
     var userCityInput = $("#inputCity").val();
     console.log(userCityInput)
+
+    var newLI = $("<li>")
+
+    newLI.text($("#inputCity").val())
+    newLI.addClass("list-group-item disabled")
+
+    $(".list-group").append(newLI)
 
     getCity(userCityInput)
 
